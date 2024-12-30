fbaefee7411c0522fb96cd555b7923cdce63c557
fruitfulninja <wuwenjun0831@gmail.com>
2024-12-30 15:10:56 +0800

    update

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -10,3 +10,22 @@ export const createFuzzySearcher = (items) => {
     minMatchCharLength: 2
   });
 };
+
+export const performSearch = (data, searchTerm, typeFilter, levelFilter, fuseOptions) => {
+  const fuse = new Fuse(data, fuseOptions);
+  let result = data;
+
+  if (searchTerm) {
+    result = fuse.search(searchTerm).map(({ item }) => item);
+  }
+
+  if (typeFilter) {
+    result = result.filter(item => item.type === typeFilter);
+  }
+
+  if (levelFilter) {
+    result = result.filter(item => item.level === levelFilter);
+  }
+
+  return result;
+};
